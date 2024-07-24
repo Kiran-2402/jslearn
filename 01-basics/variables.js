fd4ce69bcfafd5553f86d90e900c3931d3fdc860
Kiran-2402 <deepsoul1430@gmail.com>
2024-07-24 19:13:07 +0000

    learned variables

---
+++ 01-basics/variables.js
@@ -0,0 +1,15 @@
+let accountId = 1221
+let accountName = "Kedhar"
+let accountPassword = "1222"
+let accountState = true
+
+let change = "done"
+
+ {
+    change = "not done"
+    let notAccess = "true"
+    console.log(notAccess)
+ }
+//  change = "Kitne baar change hoga"
+// console.log(accountId);
+console.table([accountId,accountName,accountPassword,accountState,change,notAccess])
