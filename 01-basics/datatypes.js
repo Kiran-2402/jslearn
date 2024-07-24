319be4090db3fc3080783de3813843e70b54f888
Kiran-2402 <deepsoul1430@gmail.com>
2024-07-24 19:53:05 +0000

    learned datatypes

---
+++ 01-basics/datatypes.js
@@ -0,0 +1,17 @@
+let name1 = "Kedhar"
+let age = 22
+let married = false
+let children = undefined
+let ageOfMarry = null
+
+let largeNumber = 444444444444444444
+
+// console.table([name1,age,married,children,ageOfMarry,largeNumber]);
+console.log(typeof name1)
+console.log(typeof age)
+console.log(typeof married)
+console.log(typeof children)
+console.log(typeof ageOfMarry)
+console.log(typeof largeNumber)
+
+console.log(name1+" and age is "+age) // printing in one line
