8213cd981d7a0bbada84d60fe5c0a1e2da2580c0
Kiran-2402 <deepsoul1430@gmail.com>
2024-07-25 10:34:45 +0000

    Some basic overview on Operators

---
+++ 01-basics/ConvertingDatatypes.js
@@ -0,0 +1,52 @@
+// let score = 33;
+// console.log(score);
+// let score1 = "33"
+// console.log(score1);
+// console.log(typeof score1)
+// let numberConvert = Number(score1);
+// console.log(typeof (numberConvert))
+
+// let number = "33Abc"
+// number = Number(number);
+// console.log(number);
+
+// let value = null
+// value = Number(value)
+// console.log(value)
+
+// let score = undefined
+// score = Number(score)
+// console.log(typeof score)
+
+// let number = "Kedhar"
+// number = Boolean(number)
+// console.log(number)
+
+// let number = 33
+// console.log(typeof number)
+// number = String(number)
+// console.log(typeof number)
+
+/* ******************Operations* ********************* */
+
+// let value = 3
+// value = -value
+// console.log(value)
+
+// let name1 = "Kedhar"
+// let name2 = " Kiran"
+// // console.log(name1+name2)
+
+// // console.log("2" +1)
+// // console.log( typeof (2+1+"122"))
+// let num = 2+1+"122"
+// num = Number(num)
+// console.log(typeof num+" " +num)
+// // console.log(typeof )
+
+// console.group(+true)
+// console.log(+"       ")
+
+let num1,num2,num3
+num1 = num2 = num3 = 100
+console.log(num1,num2,num3)
