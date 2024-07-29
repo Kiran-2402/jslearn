655e91d278a3391c4813c06b91192df170bdd59b
Kiran-2402 <deepsoul1430@gmail.com>
2024-07-29 13:43:48 +0000

    Learned Number and Maths

---
+++ Number&Maths.js
@@ -0,0 +1,31 @@
+// const score = 400
+// const balance = new Number(400)
+// console.log(balance.toString())
+// console.log(typeof balance)
+
+// const otherNumber = 123.8566
+// console.log(otherNumber.toPrecision(5));
+
+// const _1Lakh = 100000;
+// console.log(_1Lakh.toLocaleString('en-IN'))
+// console.log(Number.MAX_VALUE)
+// console.log(Number.MIN_VALUE)
+// console.log(Number.MAX_SAFE_INTEGER)
+// console.log(Number.MIN_SAFE_INTEGER)
+// console.log(Number.EPSILON)
+
+// Math Operations
+
+// console.log(Math);
+// console.log(Math.abs(-5))
+// console.log(Math.round(4.5))
+// console.log(Math.ceil(4.1))
+// console.log(Math.floor(4.9))
+// console.log(Math.max(2,5,66,3,1,7))
+
+// console.log((Math.random()*10)+1)
+
+// let num1 = 10;
+// let num2 = 20;
+// console.log(Math.abs(num1-num2));
+// console.log(Math.floor(Math.random()*(num2-num1+1))+num1)
