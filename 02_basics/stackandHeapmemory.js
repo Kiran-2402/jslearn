ba9f32d378fece0c50b2f0738369c907f017ff08
Kiran-2402 <deepsoul1430@gmail.com>
2024-07-29 12:24:33 +0000

    value and reference pass learned

---
+++ 02_basics/stackandHeapmemory.js
@@ -0,0 +1,14 @@
+// let name1 = "Kedhar"
+// let name2 = name1;
+// name2 = "Kiran"
+// console.log(name1)
+// console.log(name2);
+
+let obj1 = {
+    name:"Kedhar",
+    email : "chkedharkiran@gmail.com"    
+}
+console.log(obj1.email)
+let obj2 = obj1;
+obj2.email = "anonymous@yahoo.co.in"
+console.log(obj1.email)
