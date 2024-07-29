812f3b6be109b1c2d560eaa42194e7c8897814d0
Kiran-2402 <deepsoul1430@gmail.com>
2024-07-29 12:14:00 +0000

    Primitive Datatypes learned

---
+++ 02_basics/Datatypes.js
@@ -0,0 +1,30 @@
+// const score = 100;
+// const scoreValue = 100.3
+// let username;
+// let username2 = "Ch kedhar Kiran";
+// console.log(typeof score);
+// console.log(typeof scoreValue);
+// console.log(username);
+// console.log(username2);
+
+// const id = '123'
+// const id1 = '123'
+// console.log(id===id1);
+
+// const bigNumber = BigInt(34565443357665435);
+// console.log(typeof bigNumber)
+
+// const names = ["Kedhar","Kiran","Kinnu",22]
+// console.log( typeof names)
+
+// let object = {
+//     name:"Kedhar",
+//     age:22
+// }
+// console.log(typeof object)
+
+function functionName(name) {
+    console.log(`My name is ${name}`)
+}
+functionName("Kedhar")
+console.log(typeof functionName)
