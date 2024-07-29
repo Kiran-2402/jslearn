421d474a6fb2834b14f17bb352d9ca24bba3b243
Kiran-2402 <deepsoul1430@gmail.com>
2024-07-29 13:09:56 +0000

    learned some string operations

---
+++ Strings/String.js
@@ -0,0 +1,38 @@
+// let name1 = "Kedhar"
+// let name2 = new String("Kedhar")
+// console.log(name1===name2)
+
+// String interpolation
+// let name = "CH KEDHAR KIRAN"
+// let instagram = "____.kinnu.____"
+// console.log(`Hello I'm ${name} and my instagram id is ${instagram} `)
+
+const myName = new String("chkedharkiran")
+// console.log(myName.__proto__);
+// console.log(myName.length)
+// console.log(myName.toUpperCase())
+// console.log(myName.toLowerCase)
+// console.log(myName.charAt(2))
+// console.log(myName.indexOf('a'))
+// console.log(myName.substring(0,10))
+
+// const anotherName = myName.substring(0,6);
+// console.log(anotherName)
+
+// const anotherName2 = myName.slice(-11,-5)
+// console.log(anotherName2)
+
+// const trimExample = "             KEdahrKiran02    ,                ";
+// console.log(trimExample.trim())
+
+// let url = "https://kedahr.com/kedahr%20kiran"
+// url = url.replace('%20','-')
+// url = url.replace('kedahr','kedhar')
+// console.log(url.includes('%20'))
+// console.log(url)
+
+// const name = "ch-kedhar-kiran"
+// console.log(name.split('-'))
+
+const details = "chkedhar,kiran"
+console.log(details.split(','));
